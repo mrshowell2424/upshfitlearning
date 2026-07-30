05d6eb62368266c37d6286ade0bfae81d9d98887
Stephanie Howell <stephanie@Mac.localdomain>
2026-07-30 19:05:43 -0400

    Make database module build-time safe

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -4,9 +4,19 @@ import * as schema from "./schema";
 
 const connectionString = process.env.DATABASE_URL;
 
-if (!connectionString) {
-  throw new Error("DATABASE_URL environment variable is required");
+let db: any;
+
+if (connectionString) {
+  const client = postgres(connectionString);
+  db = drizzle(client, { schema });
+} else {
+  // During build, DATABASE_URL may not be available
+  // Create a stub that will fail at runtime if actually used
+  db = new Proxy({}, {
+    get: () => {
+      throw new Error("DATABASE_URL environment variable is required");
+    }
+  });
 }
 
-const client = postgres(connectionString);
-export const db = drizzle(client, { schema });
+export { db };
